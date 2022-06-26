876868f948c8dfbc5aa159c3fc2c11a6f7ba568f
Alex G <hi+github@mavenblueprint.com>
2022-06-26 11:06:54 -0400

    Add the initial app created by Xcode

---
+++ DnD-App-Example/DnD-App-Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DnD-App-Example
+//
+//  Created by AlexG on 6/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DnD-App-Example/DnD-App-Example/DnD_App_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnD_App_ExampleApp.swift
+//  DnD-App-Example
+//
+//  Created by AlexG on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DnD_App_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
